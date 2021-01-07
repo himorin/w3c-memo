32534e0af5985e6c95f45cc95335d96593f91779
Atsushi Shimono <atsushi@himor.in>
2021-01-07 12:04:30 +0900

    added simple display for report.json

---
+++ github/listrepo.js
@@ -0,0 +1,44 @@
+const w3c_report = 'https://w3c.github.io/validate-repos/report.json';
+
+var w3c_data;
+var w3c_group;
+
+function LoadData() {
+  fetch(w3c_report, { mode: 'cors'})
+  .then(function(response) {
+    if (response.ok) {return response.json(); }
+    throw Error("Returned response for data: " + response.status);
+  }).then(function(data) {
+    w3c_data = data.repos;
+    w3c_group = data.groups;
+  });
+  document.getElementById('group_submit').addEventListener('click', ListRepositories);
+}
+window.addEventListener('load', LoadData);
+
+function ListRepositories() {
+  var gid = document.getElementById('group_id').value;
+  var out = '';
+
+  var ginfo = '';
+  if (w3c_group[gid]) {
+    ginfo = w3c_group[gid]['name'] + ' / ' + w3c_group[gid]['type'];
+  }
+  document.getElementById('group_info').innerText = ginfo;
+
+  w3c_data.forEach(elem => {
+    if (elem.w3c && (elem.w3c.group == gid)) {
+      out += '<tr>';
+      out += '<td>' + elem.owner.login + '/' + elem.name + '</td>';
+      out += '<td>' + elem.createdAt + '</td>';
+      out += '<td>' + elem.homepageUrl + '</td>';
+      out += '<td>' + elem.isPrivate + '</td>';
+      out += '<td>' + elem.defaultBranch.name + '</td>';
+      out += '<td>' + elem.w3c['repo-type'][0] + '</td>';
+      out += '</tr>';
+    }
+  });
+
+  document.getElementById('repos_list').innerHTML = out;
+}
+
