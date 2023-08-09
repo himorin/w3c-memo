fcec2d8e85f8fba03cef3e1e3199cafa1cd262c4
Atsushi Shimono <atsushi@himor.in>
2023-08-09 22:51:25 +0900

    about gh token

--- github/team/gh-api.sh
+++ github/team/gh-api.sh
@@ -1,3 +1,4 @@
+# regenerate token from https://github.com/settings/tokens
 # echo 'ghp_XXX' | gh auth login --with-token
 gh api -H 'Accept: application/vnd.github.v3+json' "/orgs/w3c/teams/w3c-group-87846-members/members?per_page=100" > w3c-87846.json
 gh api -H 'Accept: application/vnd.github.v3+json' "/orgs/w3c/teams/w3c-group-109735-members/members?per_page=100" > w3c-109735.json
