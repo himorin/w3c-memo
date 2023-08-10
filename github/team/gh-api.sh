00a45bf3986a42ccaa6176454ed36c008848cc7c
Atsushi Shimono <atsushi@himor.in>
2023-08-10 17:47:49 +0900

    updated gh cli

--- github/team/gh-api.sh
+++ github/team/gh-api.sh
@@ -1,7 +1,7 @@
 # regenerate token from https://github.com/settings/tokens
 # echo 'ghp_XXX' | gh auth login --with-token
-gh api -H 'Accept: application/vnd.github.v3+json' "/orgs/w3c/teams/w3c-group-87846-members/members?per_page=100" > w3c-87846.json
-gh api -H 'Accept: application/vnd.github.v3+json' "/orgs/w3c/teams/w3c-group-109735-members/members?per_page=100" > w3c-109735.json
-gh api -H 'Accept: application/vnd.github.v3+json' "/orgs/immersive-web/teams/w3c-group-87846-members/members?per_page=100" > iwwg-87846.json
-gh api -H 'Accept: application/vnd.github.v3+json' "/orgs/immersive-web/teams/w3c-group-109735-members/members?per_page=100" > iwwg-109735.json
+gh api -H 'Accept: application/vnd.github+json' "/orgs/w3c/teams/w3c-group-87846-members/members?per_page=100" > w3c-87846.json
+gh api -H 'Accept: application/vnd.github+json' "/orgs/w3c/teams/w3c-group-109735-members/members?per_page=100" > w3c-109735.json
+gh api -H 'Accept: application/vnd.github+json' "/orgs/immersive-web/teams/w3c-group-87846-members/members?per_page=100" > iwwg-87846.json
+gh api -H 'Accept: application/vnd.github+json' "/orgs/immersive-web/teams/w3c-group-109735-members/members?per_page=100" > iwwg-109735.json
 ls -1 *json | awk '{print "./print_login.py", $1,"|sort > " $1 "-list"}'
