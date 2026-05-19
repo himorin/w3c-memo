d2f473a65b2268c6a7c4c5ec4ca65543d2535151
Atsushi Shimono <atsushi@himor.in>
2026-05-20 00:26:14 +0900

    github/disposition-comments: updated with file output, start date

--- github/disposition-comments/README.md
+++ github/disposition-comments/README.md
@@ -10,10 +10,14 @@ scripts for generating static archive from github discussions.
   * `gh_pat`: GitHub access token (personal access token), to extend limit of GitHub API limit
   * `hlevel`: heading level for issue top and to separate comments (one level down)
 * `generate_comments.py`: generate html archive from single issue
-  * usage: `<script> <github url>`
+  * usage: `<script> (file) <github url> (<from date>)`
+    * if `file` is inserted, load `<github url>` from specified file, one URL per line
+    * if `<from date>` (in YYYYMMDD) is specified, only comments after specified date (in UTC) will be output
   * output html, via markdown conversion API called per each comment
     * not taking way first to composite all comments w/ separation by `###` and to convert, difficulty to seek separation point when comment has heading markup
-  * output content
+  * output content - to file
+    * name of file : `<org>-<repo>-<issue id>(-from-<from date>).html`
+      * output file will be newly created (or overrided)
     * heading: hX including repository ID with link
     * issue metadata
     * comments
--- github/disposition-comments/generate_comments.py
+++ github/disposition-comments/generate_comments.py
@@ -3,6 +3,7 @@
 import base_routines
 import json
 import sys
+import datetime
 
 DEF_ISSUE_HEAD = "<h[HL]>Comments provided to issue <a href=\"[IURL]\">[IREPO] #[IID]: [ITITLE]</a></h[HL]>"
 DEF_ISSUE_META = "<ul><li>Status: [ISTAT]</li><li>Created: <time datetime=\"[ICREDT]\">[ICREDAY]</time></li></ul>"
@@ -30,13 +31,25 @@ def PackCommentUpdate(c_obj, c_dat):
     c_dat['CEDITEDDT'] = c_obj.updated_at.isoformat()
   return c_dat
 
-def PrintIssue(c_cnf, c_gh, url):
+def PrintIssue(c_cnf, c_gh, url, fromdate = None):
   c_icfg = url.split('/') # 3:org, 4:repo, 6:id
   c_repo_name = "%s/%s" % (c_icfg[3], c_icfg[4])
   c_repo = c_gh.get_repo(c_repo_name)
   c_issue = c_repo.get_issue(number = int(c_icfg[6]))
+  c_of = "%s-%s-%s" % (c_icfg[3], c_icfg[4], c_icfg[6])
+  c_from = None
+  if fromdate is not None:
+    c_of += "-%s" % fromdate
+    c_from = datetime.datetime(int(fromdate[0:4]), int(fromdate[4:6]), int(fromdate[6:8])).replace(tzinfo = datetime.timezone.utc)
+  c_of += ".html"
 
-  print("\n<!--\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n-->\n\n" % (c_issue.html_url, c_issue.number, c_issue.state, c_issue.title, c_issue.user.login, c_issue.created_at, c_issue.updated_at, c_issue.closed_at))
+  try:
+    c_fh = open(c_of, 'w')
+  except IOError as e:
+    print("Could not open file to write: %s" % (c_of))
+    return None
+
+  print("\n<!--\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n-->\n\n" % (c_issue.html_url, c_issue.number, c_issue.state, c_issue.title, c_issue.user.login, c_issue.created_at, c_issue.updated_at, c_issue.closed_at), file = c_fh)
   c_dat = {}
   c_dat['HL'] = str(c_cnf['hlevel'] - 1)
   c_dat['IURL'] = c_issue.html_url
@@ -46,36 +59,45 @@ def PrintIssue(c_cnf, c_gh, url):
   c_dat['ISTAT'] = c_issue.state
   c_dat['ICREDT'] = c_issue.created_at.isoformat()
   c_dat['ICREDAY'] = c_issue.created_at.strftime('%Y-%m-%d')
-  print(base_routines.ReplaceBracket(DEF_ISSUE_HEAD, c_dat))
-  print(base_routines.ReplaceBracket(DEF_ISSUE_META, c_dat))
+  print(base_routines.ReplaceBracket(DEF_ISSUE_HEAD, c_dat), file = c_fh)
+  print(base_routines.ReplaceBracket(DEF_ISSUE_META, c_dat), file = c_fh)
 
   c_dat = PackCommentUpdate(c_issue, c_dat)
   c_dat['COMMENT'] = c_gh.render_markdown(text = c_issue.body, context = c_repo)
-  print(base_routines.ReplaceBracket(DEF_COMMENT_INIT, c_dat))
-  print(base_routines.ReplaceBracket(DEF_COMMENT_BODY, c_dat))
+  print(base_routines.ReplaceBracket(DEF_COMMENT_INIT, c_dat), file = c_fh)
+  print(base_routines.ReplaceBracket(DEF_COMMENT_BODY, c_dat), file = c_fh)
 
 #  c_dat = c_issue.get_comments().totalCount
 #  c_dat = c_issue.get_comments().get_page(0)
   for c_comment in c_issue.get_comments():
-    print("\n\n<!--\n%s\n%s\n%s\n%s\n-->\n" % (c_comment.id, c_comment.user.login, c_comment.created_at, c_comment.updated_at))
+    if (c_from is None) or (c_comment.updated_at < c_from):
+      continue
+    print("\n\n<!--\n%s\n%s\n%s\n%s\n-->\n" % (c_comment.id, c_comment.user.login, c_comment.created_at, c_comment.updated_at), file = c_fh)
     c_dat = {}
     c_dat['HL'] = str(c_cnf['hlevel'])
     c_dat = PackCommentUpdate(c_comment, c_dat)
     c_dat['COMMENT'] = c_gh.render_markdown(text = c_comment.body, context = c_repo)
-    print(base_routines.ReplaceBracket(DEF_COMMENT_HEAD, c_dat))
-    print(base_routines.ReplaceBracket(DEF_COMMENT_BODY, c_dat))
+    print(base_routines.ReplaceBracket(DEF_COMMENT_HEAD, c_dat), file = c_fh)
+    print(base_routines.ReplaceBracket(DEF_COMMENT_BODY, c_dat), file = c_fh)
+
+  c_fh.close()
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
-    raise Exception("Invalid parameter: command <issue_url> / https://github.com/<org>/<repo>/issues/<id>")
+    raise Exception("Invalid parameter: command (file) <issue_url> (from_date) / URL = https://github.com/<org>/<repo>/issues/<id>")
   c_cnf = base_routines.LoadConfig()
   c_gh = base_routines.AuthGhapi(c_cnf['gh_pat'])
+  c_from = None
   if sys.argv[1] == 'file':
+    if len(sys.argv) >= 4:
+      c_from = sys.argv[3]
     with open(sys.argv[2], 'r') as flist:
       for c_url in flist.readlines():
-        PrintIssue(c_cnf, c_gh, c_url.strip())
+        PrintIssue(c_cnf, c_gh, c_url.strip(), c_from)
   else:
-    PrintIssue(c_cnf, c_gh, sys.argv[1])
+    if len(sys.argv) >= 3:
+      c_from = sys.argv[2]
+    PrintIssue(c_cnf, c_gh, sys.argv[1], c_from)
 
 # no cache save, consider json dump for object of pygithub
 #  base_routines.SaveCache()
